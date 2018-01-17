a951a24acd43d1fea40d3c18b2a252a0f17a1a03
alexandracch <alexa_theprinces@hotmail.com>
2018-01-17 16:50:32 -0500

    agregando el foto, nombre y email del usuario

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
   // Initialize collapse button
   $('.button-collapse').sideNav();
+  $('#photo').attr('src', localStorage.photo);
+  $('#name').append(localStorage.name);
+  $('#email').append(localStorage.email);
 });
