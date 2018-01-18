ed6b33abc6161874ace01e895a418a53e5a9530d
YoannaHS <yoannahernandez.s@gmail.com>
2018-01-18 16:49:57 -0500

    Añadiendo vista de profileuser

---
+++ js/profileuser.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  // Iniciando parallax del gif de portada
+    $('.parallax').parallax();
+    // Iniciando boton para mostrar info de perfil
+    $('.tap-target').tapTarget('open');
+    $('.tap-target').tapTarget('close');
+  });
