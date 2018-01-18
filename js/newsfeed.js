6f215aa12114e89cab7fd3242fcbc486a19fad85
AraceliCumen <aycuevam@gmail.com>
2018-01-18 15:58:36 -0500

    Actualizando vista search y newsfeed

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Inicializamos material box
   $('.materialboxed').materialbox();
 
@@ -22,25 +22,34 @@ $(document).ready(function() {
   // var containerMovies4 = $('#container-movie-4');
 
   for (var i = 0; i < arrMovies.length; i++) {
-
     if (i < 3) {
-      containerMovies1.prepend('<div class="col s4 container-flex-column"><img id="movie' + i + '" src="" alt="movies-API-OMDB" class="materialboxed imgs-gallery responsive-img"><h6 class=center-align id="name-movie' + i + '">Nombre Película</h6><a class="waves-effect waves-light btn modal-trigger" href="#modal-movie">Ver Datos</a></div>');
+      containerMovies1.prepend('<div class="col s4 container-flex-column"><img id="movie' + i + '" src="" alt="movies-API-OMDB" class="materialboxed imgs-gallery responsive-img"><h6 class=center-align id="name-movie' + i + '">Nombre Película</h6><a class="waves-effect waves-light btn modal-trigger btn-modal" href="#modal-movie">Ver Datos</a></div>');
     }
     if (i >= 3 && i < 6) {
-      containerMovies2.prepend('<div class="col s4 container-flex-column"><img id="movie' + i + '" src="" alt="movies-API-OMDB" class="materialboxed imgs-gallery responsive-img"><h6 class=center-align id="name-movie' + i + '">Nombre Película</h6><a class="waves-effect waves-light btn modal-trigger" href="#modal-movie">Ver Datos</a></div>');
+      containerMovies2.prepend('<div class="col s4 container-flex-column"><img id="movie' + i + '" src="" alt="movies-API-OMDB" class="materialboxed imgs-gallery responsive-img"><h6 class=center-align id="name-movie' + i + '">Nombre Película</h6><a class="waves-effect waves-light btn modal-trigger btn-modal" href="#modal-movie">Ver Datos</a></div>');
     }
     if (i >= 6 && i < 9) {
-      containerMovies3.prepend('<div class="col s4 container-flex-column"><img id="movie' + i + '" src="" alt="movies-API-OMDB" class=" materialboxed imgs-gallery responsive-img"><h6 class=center-align id="name-movie' + i + '">Nombre Película</h6><a class="waves-effect waves-light btn modal-trigger" href="#modal-movie">Ver Datos</a></div>');
+      containerMovies3.prepend('<div class="col s4 container-flex-column"><img id="movie' + i + '" src="" alt="movies-API-OMDB" class=" materialboxed imgs-gallery responsive-img"><h6 class=center-align id="name-movie' + i + '">Nombre Película</h6><a class="waves-effect waves-light btn modal-trigger btn-modal" href="#modal-movie">Ver Datos</a></div>');
     }
     // if (i >= 9 && i < 12) {
     //   containerMovies4.prepend('<div class="col s4 container-flex-column"><img id=movie' + i + ' + src="" alt="movies-API-OMDB"class="imgs-gallery responsive-img"><h6 id=name-movie' + i + ' >Nombre Película</h6>');
     // }
     // apicall(arrMovies[i], '#movie' + i, '#name-movie' + i, '#td-year', '#td-time', '#td-repart', '#td-genre', '#name-movie1', '#movie1');
-    // $('#td-year').text('');
-    // $('#td-time').text('');
-    // $('#td-repart').text('');
-    // $('#td-genre').text('');
-    apicall(arrMovies[i], '#movie' + i, '#name-movie' + i, '#td-year', '#td-time', '#td-repart', '#td-genre');
+
+    // $('.btn-modal').on('click', function() {
+    //   // $('#td-year').removeAttr('id');
+    //   $('#td-year').attr('id', 'td-year' + i);
+    //   // console.log('td-year' + i);
+    //   // $('#td-time').removeAttr('id');
+    //   $('#td-time').attr('id', 'td-time' + i);
+    //   // $('#td-repart').removeAttr('id');
+    //   $('#td-repart').attr('id', 'td-repart' + i);
+    //   // $('#td-genre').removeAttr('id');
+    //   $('#td-genre').attr('id', 'td-genre' + i);
+    // });
+
+    apicall(arrMovies[i], '#movie' + i, '#name-movie' + i);
+
     //
     // $('#td-year').text('');
     // $('#td-time').text('');
@@ -53,18 +62,31 @@ $(document).ready(function() {
   // llamando API OMDB:
   // , idYear, idTime, idRepart, idGenre, idNameMovie, idImgMovie
   // &apikey=a1792c9b
-  function apicall(nameMovie, idImg, idNameMovie, idYear, idTime, idRepart, idGenre) {
+
+
+  function apicall(nameMovie, idImg, idNameMovie) {
     $.getJSON('http://www.omdbapi.com/?t=' + nameMovie + '&apikey=a1792c9b').then(function(response) {
       console.log(response);
+      // arrNameMovies.push = response.Title;
+      // console.log(arrNameMovies);
+      // console.log(idYear);
       // var name = response.Title;
       $(idImg).attr('src', response.Poster);
       $(idNameMovie).text(response.Title);
-      $(idYear).text(response.Year);
-      $(idTime).text(response.Runtime);
-      $(idRepart).text(response.Actors);
-      $(idGenre).text(response.Genre);
-      // $(idNameMovie).text(name);
-      // $(idImgMovie).attr('src', response.Poster);
+
+      $('.btn-modal').on('click', function() {
+        console.log(response);
+        $('#td-year').text(response.Year);
+        $('#td-time').text(response.Runtime);
+        $('#td-repart').text(response.Actors);
+        $('#td-genre').text(response.Genre);
+        $('#name-movie').text(response.Title);
+        $('#movie-img').attr('src', response.Poster);
+      });
     });
   };
+
+  $('#btn-reload').on('click', function() {
+    location.reload();
+  });
 });
