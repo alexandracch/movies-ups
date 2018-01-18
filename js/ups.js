bff2384053293d5d3bad85eb0f538a90376d9869
Nefelijm <sistemasnik20@gmail.com>
2018-01-18 09:43:37 -0500

    Añadiendo funcionalidad a la vista ups

--- js/ups.js
+++ js/ups.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
   $('.slider').slider();
   // Initialize collapse button
   $('.button-collapse').sideNav();
+  // bar
+  setInterval(function() {
+    $('#podium1').animate({'width': '0%'}).animate({'width': '90%'}, 1000)
+      .delay(7000).animate({'opacity': '0'}, 1000)
+      .animate({'width': '0%'}).animate({'opacity': '1'});
+  }, 100);
+  setInterval(function() {
+    $('#podium2').animate({ 'width': '0%' }).animate({ 'width': '75%' }, 1000)
+      .delay(7000).animate({ 'opacity': '0' }, 1000)
+      .animate({ 'width': '0%' }).animate({ 'opacity': '1' });
+  }, 400);
+  setInterval(function() {
+    $('#podium3').animate({ 'width': '0%' }).animate({ 'width': '65%' }, 1000)
+      .delay(7000).animate({ 'opacity': '0' }, 1000)
+      .animate({ 'width': '0%' }).animate({ 'opacity': '1' });
+  }, 600);
 });
